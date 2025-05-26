491dcc9057cf70270fa280eacdd78f5201874b29
Satyam Rajput <satyamrajput081998@gmail.com>
2025-05-26 05:46:12 -0400

    add some features

--- server.js
+++ server.js
@@ -22,12 +22,34 @@ app.get("/", (req, res) => {
 io.on("connection", (socket) => {
   console.log("A user connected:", socket.id);
 
+  // Broadcast drawing data
   socket.on("draw", (data) => {
     socket.broadcast.emit("draw", data);
   });
 
+  // Broadcast text data
   socket.on("text", (textData) => {
     socket.broadcast.emit("text", textData);
+  });
+
+  // Broadcast clear event
+  socket.on("clear", () => {
+    socket.broadcast.emit("clear");
+  });
+
+  // Broadcast cursor data
+  socket.on("cursor", (data) => {
+    socket.broadcast.emit("cursor", data);
+  });
+
+  // Broadcast pointer event
+  socket.on("pointer", (data) => {
+    io.emit("pointer", data);
+  });
+
+  // New event for shared text selection
+  socket.on("sharedSelection", (selectionData) => {
+    socket.broadcast.emit("sharedSelection", selectionData); // Broadcast the selected text to other users
   });
 
   socket.on("disconnect", () => {
